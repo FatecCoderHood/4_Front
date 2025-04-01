ec134d1e83f71fb3ddeff3ef26c3bfc0bcd6cf9a
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2025-04-01 00:48:14 -0300

    chore <VIS-23>: Implement redirect to 404 when undefined route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,66 @@
-//import Dashboard from '@/pages/Dashboard.vue'; // Exemplo de uma página protegida
-import Inicial from '@/pages/Inicial.vue'; // Ajuste o caminho conforme necessário
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'
+import Inicial from '@/pages/Inicial.vue'
+import Users from '@/pages/Users.vue'
+import NotFound from '@/pages/NotFound.vue'
 
 const routes = [
-  { path: '/', redirect: '/inicial' }, // Rota padrão leva para a tela inicial
-  { path: '/inicial', component: Inicial },
-  //{ path: '/dashboard', component:Dashboard, meta: { requiresAuth: true } },
+  {
+    path: '/Inicial',
+    component: Inicial,
+    meta: {
+      title: 'Login'
+    }
+  },
+  {
+    path: '/Users',
+    component: Users,
+    meta: {
+      title: 'Usuários',
+      requiresAuth: true
+    }
+  },
+
+  // Catch-all route for undefined paths (404 page)
+  { path: '/:pathMatch(.*)*', component: NotFound }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
 
-// Proteção de rotas (impede acesso sem login)
+// Navigation Guard to check for authentication
 router.beforeEach((to, from, next) => {
-  const isAuthenticated = !!localStorage.getItem('user')
+  const isAuthenticated = !!localStorage.getItem('user') // Check if user is authenticated
 
   if (to.meta.requiresAuth && !isAuthenticated) {
-    next('/inicial') // Redireciona para login se não estiver autenticado
+    // Redirect to login page if the route requires auth and the user is not authenticated
+    next({
+      path: '/inicial',
+      query: { redirect: to.fullPath }, // Store the attempted route
+    });
   } else {
-    next()
+    next() // Allow navigation to the route
   }
+})
+
+// Workaround for https://github.com/vitejs/vite/issues/11804
+router.onError((err, to) => {
+  if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
+    if (!localStorage.getItem('vuetify:dynamic-reload')) {
+      console.log('Reloading page to fix dynamic import error')
+      localStorage.setItem('vuetify:dynamic-reload', 'true')
+      location.assign(to.fullPath)
+    } else {
+      console.error('Dynamic import error, reloading page did not fix it', err)
+    }
+  } else {
+    console.error(err)
+  }
+})
+
+router.isReady().then(() => {
+  localStorage.removeItem('vuetify:dynamic-reload')
 })
 
 export default router
