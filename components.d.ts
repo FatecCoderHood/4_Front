cdea92ab897818e1ae869d1d1d3f2868e998cdf3
JuanCursino <143050764+JuanCursino@users.noreply.github.com>
2025-04-27 16:35:16 -0300

    feat <VIS-92> [Frontend] Implementar tela de avaliação (Aprovado | Rejeitado) da inspeção
    Descrição da implementação:
    
    - Botão de aprovar ou recusar

--- components.d.ts
+++ components.d.ts
@@ -16,6 +16,7 @@ declare module 'vue' {
     FarmList: typeof import('./src/components/Farms/FarmList.vue')['default']
     FarmsMenu: typeof import('./src/components/Map/FarmsMenu.vue')['default']
     FarmTableActions: typeof import('./src/components/Farms/FarmTableActions.vue')['default']
+    FarmViewDialog: typeof import('./src/components/Farms/FarmViewDialog.vue')['default']
     GeoJSONLayer: typeof import('./src/components/Map/GeoJSONLayer.vue')['default']
     GeoJsonProcessor: typeof import('./src/components/Farms/GeoJsonProcessor.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
@@ -25,5 +26,6 @@ declare module 'vue' {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SideBar: typeof import('./src/components/SideBar.vue')['default']
+    StatusActions: typeof import('./src/components/Map/StatusActions.vue')['default']
   }
 }
